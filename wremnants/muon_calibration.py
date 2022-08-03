5b143da0c81585a4e66690e58293a09987165b1b
Tianyu Justin Yang <tyjyang@mit.edu>
2022-08-03 11:55:03 +0200

    attempt to make pt smearing work

--- wremnants/muon_calibration.py
+++ wremnants/muon_calibration.py
@@ -82,8 +82,10 @@ def define_corrected_muons(df, helper):
     df = df.Define("Muon_correctedMom4Charge", helper, ["Muon_cvhbsPt", "Muon_cvhbsEta", "Muon_cvhbsPhi", "Muon_cvhbsCharge", "Muon_cvhmergedGlobalIdxs", "Muon_cvhbsJacRef"])
 
     # split into individual vectors
-    df = df.Define("Muon_correctedPt", "ROOT::VecOps::RVec<float> res(Muon_correctedMom4Charge.size()); std::transform(Muon_correctedMom4Charge.begin(), Muon_correctedMom4Charge.end(), res.begin(), [](const auto &x) { return x.first.Pt(); } ); return res;")
-    df = df.Define("Muon_correctedEta", "ROOT::VecOps::RVec<float> res(Muon_correctedMom4Charge.size()); std::transform(Muon_correctedMom4Charge.begin(), Muon_correctedMom4Charge.end(), res.begin(), [](const auto &x) { return x.first.Eta(); } ); return res;")
+#    df = df.Define("Muon_correctedPt", "ROOT::VecOps::RVec<float> res(Muon_correctedMom4Charge.size()); std::transform(Muon_correctedMom4Charge.begin(), Muon_correctedMom4Charge.end(), res.begin(), [](const auto &x) { return x.first.Pt(); } ); return res;")
+    df = df.Define("Muon_correctedPt", "ROOT::VecOps::RVec<float> res(GenPart_pt.size()); double sigma = Muon_cvhbsMomCov[0][0]; std::transform(GenPart_pt.begin(), GenPart_pt.begin(), res.begin(), [&sigma] (const auto &x) { return wrem::gaussian_smearing(x, sigma);} ); return res;")
+#    df = df.Define("Muon_correctedEta", "ROOT::VecOps::RVec<float> res(Muon_correctedMom4Charge.size()); std::transform(Muon_correctedMom4Charge.begin(), Muon_correctedMom4Charge.end(), res.begin(), [](const auto &x) { return x.first.Eta(); } ); return res;")
+    df = df.Define("Muon_correctedEta", "GenPart_eta")
     df = df.Define("Muon_correctedPhi", "ROOT::VecOps::RVec<float> res(Muon_correctedMom4Charge.size()); std::transform(Muon_correctedMom4Charge.begin(), Muon_correctedMom4Charge.end(), res.begin(), [](const auto &x) { return x.first.Phi(); } ); return res;")
     df = df.Define("Muon_correctedCharge", "ROOT::VecOps::RVec<int> res(Muon_correctedMom4Charge.size()); std::transform(Muon_correctedMom4Charge.begin(), Muon_correctedMom4Charge.end(), res.begin(), [](const auto &x) { return x.second; }); return res;")
 
